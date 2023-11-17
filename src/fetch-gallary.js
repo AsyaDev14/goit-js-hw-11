81442c2aab082568038786fa7a2bcb173c88c677
AsyaDev <142207257+AsyaDev14@users.noreply.github.com>
2023-11-17 13:26:35 +0200

    fetch page = 40

--- src/fetch-gallary.js
+++ src/fetch-gallary.js
@@ -4,7 +4,7 @@ const BASE_URL = 'https://pixabay.com/api/'
 const MY_KEY = '33816653-3cca4f3926f281165d337bdaa';
 
 export const fetchGallery = async (value, page) => {
-  const res = await axios.get(`${BASE_URL}?key=${MY_KEY}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true&per_page=5&page=${page}`);
+  const res = await axios.get(`${BASE_URL}?key=${MY_KEY}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`);
   console.log("res", res);
   return res.data;
 }
